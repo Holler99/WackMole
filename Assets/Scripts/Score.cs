08749545aeb83e4fbb97402eda17ffe400533788
Zane Camp <zmcamp1306@muleriders.saumag.edu>
2023-09-21 12:09:04 -0500

    Added mole and hammer model

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    //int Score = 0;
+    //int MoleHit = 0;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
